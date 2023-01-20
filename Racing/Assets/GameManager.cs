cb6491da6506c4637a42013c6066f4d19e1d6bc2
3rr0rD1v1d3d8yZ3r0 <99330415+3rr0rD1v1d3d8yZ3r0@users.noreply.github.com>
2023-01-20 14:39:08 +0700

    Timer and FOV
    Implemented the timer and the fov change on the car.

--- Racing/Assets/GameManager.cs
+++ Racing/Assets/GameManager.cs
@@ -1,5 +1,7 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,12 +15,24 @@ public class GameManager : MonoBehaviour
 
     private GameStates gameState = GameStates.PlayState;
 
+    [Header("UI")]
     [SerializeField] private GameObject pauseUI;
     [SerializeField] private GameObject finishUI;
     [SerializeField] private GameObject playUI;
+    [SerializeField] private Text timerText;
+    [SerializeField] private Text millisecondsText;
+    [SerializeField] private TMP_Text totalTimeText;
 
+    [Header("Finish line")]
     [SerializeField] private GameObject finishLine;
     [SerializeField] private LayerMask finishLineLayerMask;
+
+
+    [Header("Timer")]
+    private float totalSeconds;
+    [SerializeField] private string s_seconds;
+    [SerializeField] private string s_minutes;
+
 
     private void Start()
     {
@@ -37,7 +51,23 @@ public class GameManager : MonoBehaviour
         {
             Finish();
         }
+
+        RunTimer();
         
+    }
+
+    void RunTimer()
+    {
+        totalSeconds += Time.deltaTime;
+
+        int minutes = Mathf.FloorToInt(totalSeconds / 60);
+        int seconds = Mathf.FloorToInt(totalSeconds - minutes * 60);
+        int milliseconds = Mathf.FloorToInt((totalSeconds - seconds) * 1000);
+
+        s_minutes = minutes / 10 > 0 ? minutes.ToString() : "0" + minutes.ToString();
+        s_seconds = seconds / 10 > 0 ? seconds.ToString() : "0" + seconds.ToString();
+
+        timerText.text = s_minutes + " : " + s_seconds;
     }
 
 
@@ -56,6 +86,7 @@ public class GameManager : MonoBehaviour
     
     private void Finish()
     {
+        totalTimeText.text = s_minutes + " : " + s_seconds;
         gameState = GameStates.WinState;
         finishUI.SetActive(true);
         playUI.SetActive(false);
